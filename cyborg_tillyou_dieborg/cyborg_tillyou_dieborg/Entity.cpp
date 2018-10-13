118c399c9c870aadb3f7730191f9ccc687abbcf1
cnpatton <botanicality@gmail.com>
2018-10-13 10:33:18 -0500

    Adding Entity class

--- cyborg_tillyou_dieborg/cyborg_tillyou_dieborg/Entity.cpp
+++ cyborg_tillyou_dieborg/cyborg_tillyou_dieborg/Entity.cpp
@@ -1,16 +1,10 @@
 #include "Entity.h"
-#include <math.h>
 
-const int Entity::DIR_UP=0, DIR_DOWN=1, DIR_LEFT=2, DIR_RIGHT=3, DIR_NONE=-1;
+const int Entity::DIR_UP=0, Entity::DIR_DOWN=1, Entity::DIR_LEFT=2, Entity::DIR_RIGHT=3, Entity::DIR_NONE=-1;
 
-Entity::Entity()
-{
-}
+list<Entity*> Entity::entities;
 
-
-Entity::~Entity()
-{
-}
+/* VIRTUAL FUNCTIONS */
 
 void Entity::update(){}// override me to do something useful
 
@@ -94,45 +88,180 @@ void Entity::update_movement()
 
 void Entity::update_collision_box()
 {
+	// center collision box 
+	collisionBox.x = x - collisionBox.w / 2;
+	collisionBox.y = y + collisionBoxYOffset;
+	collisionBox.w = collisionBoxWidth;
+	collisionBox.h = collisionBoxHeight;
 }
 
 void Entity::update_collisions()
 {
+	// Don't want to check for collisions if standing still - just implementation for now
+	if (active && collideWithSolids && (moveSpeed > 0 || pushbackAmount > 0)) {
+		// List of potential collisions
+		// this->collisionBox = union of where they were before and where they are now
+		list<Entity*> collisions;
+		for (auto entity = Entity::entities.begin(); entity != Entity::entities.end(); ++entity) {
+			if ((*entity)->active  // must be active
+				&& (*entity)->type != this->type // don't collide with same types (wall on wall or enemy on enemy)
+				&& (*entity)->solid // must be solid to collide
+				&& Entity::check_collides_with(collisionBox, (*entity)->collisionBox)) // colliders overlap 
+			{
+				collisions.push_back((*entity));
+			}
+		}
+		// if we have anything in the collisions list we have to check them properly
+		if (collisions.size() > 0) {
+			update_collision_box();
+
+			// Multisample check for collisions from where we started to where we are planning to move to
+			// Calculate sample distance we are going to travel between checks
+			float boxTravelChunk = 0;
+			if (collisionBox.w < collisionBox.h) {
+				boxTravelChunk = collisionBox.w / 4;
+			}else{
+				boxTravelChunk = collisionBox.h / 4;
+			}
+
+			// Use sampleBox to check for collisions from start point to end point moving at boxTravelChunk each sample
+			SDL_Rect sampleBox = lastCollisionBox;
+			float movementAngle = Entity::angle_between_two_rects(lastCollisionBox, collisionBox);
+
+			bool foundCollision = false;
+			while (!foundCollision) {
+				SDL_Rect intersection;
+				for (auto entity = collisions.begin(); entity != collisions.end(); ++entity) {
+					if (SDL_IntersectRect(&sampleBox, &((*entity)->collisionBox), &intersection)) {
+						// This indicates there was a collision - update!
+						foundCollision = true;
+						moveSpeed = 0;
+						moving = false;
+						// Update pushback basically the reverse angle 
+						pushbackAngle = angle_between_two_entities((*entity), this);
+						
+						if (intersection.w < intersection.h) {
+							// Check coming from left or right
+							if (lastCollisionBox.x + lastCollisionBox.w / 2 < (*entity)->collisionBox.x + (*entity)->collisionBox.w / 2) {
+								// came from left, move left for pushback
+								sampleBox.x -= intersection.w;
+							}
+							else {
+								// came from right, move right for pushback
+								sampleBox.x += intersection.w;
+							}
+						}
+						else {
+							if (lastCollisionBox.y + lastCollisionBox.h / 2 < (*entity)->collisionBox.y + (*entity)->collisionBox.h / 2) {
+								// came from up, move up for pushback
+								sampleBox.y -= intersection.h;
+							}
+							else {
+								// came from down, move down for pushback
+								sampleBox.y += intersection.h;
+							}
+						}
+					}
+				}
+				if (foundCollision || (sampleBox.x == collisionBox.x && sampleBox.y == collisionBox.y)) {
+					break;
+				}
+
+				// Move sample box to next check spot
+				if (distance_between_two_rects(sampleBox, collisionBox) > boxTravelChunk) {
+					float xMove = boxTravelChunk*(cos(movementAngle*Globals::PI / 180));
+					float yMove = boxTravelChunk*(sin(movementAngle*Globals::PI / 180));
+
+					sampleBox.x += xMove;
+					sampleBox.y += yMove;
+				}
+				else {
+					sampleBox = collisionBox;
+				}
+			}
+			if (foundCollision) {
+				// move our entity to where the sample box ended up
+				pushbackAmount = pushbackAmount / 2;
+				x = sampleBox.x + sampleBox.w / 2;
+				y = sampleBox.y - collisionBoxYOffset;
+			}
+			update_collision_box();
+		}
+	}
 }
 
+/* HELPER FUNCTIONS */
 float Entity::distance_between_two_rects(SDL_Rect & r1, SDL_Rect & r2)
 {
-	return 0.0f;
+	SDL_Point p1, p2;
+	p1.x = r1.x + r1.w / 2; // center x value of first rect
+	p1.y = r1.y + r1.h / 2; // center y value of first rect
+
+	p2.x = r2.x + r2.w / 2; // center x value of second rect
+	p2.y = r2.y + r2.h / 2; // center y value of second rect
+
+	// d = (x2 - x1)^2 + (y2 - y1)^2
+	float dist = abs(sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2)));
+	return dist;
 }
 
-float Entity::distance_between_two_entities(Entity * e1, Entity * e2)
+float Entity::distance_between_two_entities(Entity* e1, Entity* e2)
 {
-	return 0.0f;
+	return abs(sqrt(pow(e1->x - e1->x, 2) + pow(e2->y - e2->y, 2)));
 }
 
-float Entity::angle_between_two_entities(Entity * e1, Entity * e2)
+float Entity::angle_between_two_entities(Entity* e1, Entity* e2)
 {
-	return 0.0f;
+	float dx, dy;
+	float x1 = e1->x, y1 = e1->y, x2 = e2->x, y2 = e2->y;
+	
+	dx = x2 - x1;
+	dy = y2 - y1;
+
+	// angle in radians converted to degrees
+	return atan2(dy, dx) * 180 / Globals::PI;
 }
 
 bool Entity::check_collides_with(SDL_Rect cbox, SDL_Rect otherCBox)
 {
-	return false;
+	return SDL_IntersectRect(&cbox, &otherCBox, nullptr);// don't care about storing the result - just checking if it intersects
+	// other possibilities for collision is if a rectangle is inside another rectangle
 }
 
+// Convert 360 degree angle into const DIR
 int Entity::angle_to_direction(float angle)
 {
-	return 0;
+	if ((angle >= 0 && angle <= 45) || angle >= 315 && angle <= 360) {
+		return DIR_RIGHT;
+	}
+	else if(angle > 45 && angle <= 135){
+		return DIR_UP;
+	}
+	else if (angle > 135 && angle <= 225) {
+		return DIR_LEFT;
+	}
+	else {
+		return DIR_DOWN;
+	}
 }
 
-float Entity::angle_between_two_points(float e1X, float e1Y, float e2X, float e2Y)
+float Entity::angle_between_two_points(float px1, float py1, float px2, float py2)
 {
-	return 0.0f;
+	float dx = px2 - px1;
+	float dy = py2 - py1;
+	return atan2(dx, dy) * 180 / Globals::PI;
 }
 
-float Entity::angle_between_two_rects(SDL_Rect & r1, SDL_Rect & r2)
+float Entity::angle_between_two_rects(SDL_Rect& r1, SDL_Rect& r2)
 {
-	return 0.0f;
+	// center of first rectangle
+	float px1 = r1.x + (r1.w / 2);
+	float py1 = r1.y + (r1.h / 2);
+	// center of second rectangle
+	float px2 = r2.x + (r2.w / 2);
+	float py2 = r2.y + (r2.h / 2);
+
+	return angle_between_two_points(px1, py1, px2, py2);
 }
 
 bool Entity::entity_compare(const Entity * const & entity, const Entity * const & otherEntity)
